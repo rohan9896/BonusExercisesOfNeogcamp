decaa8924d40c4be7a91f195531a7ac861ae537d
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-04 01:19:24 +0530

    Create: PrimeNumber.js

---
+++ PrimeNumber.js
@@ -0,0 +1,29 @@
+var chalk = require('chalk');
+var readlinesync = require('readline-sync');
+
+function isPrime(n){
+    if(n == 1){
+        return false;
+    }
+
+    for(var i = 2; i*i < n; i++){
+
+        if(n%i == 0){
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+// console.log(isPrime(13))
+
+var userInput = readlinesync.question(chalk.bgBlue("Enter the number: "));
+
+if(isPrime(userInput)){
+    console.log(chalk.blue(userInput+" is a prime number"));
+}
+else{
+    console.log(chalk.red(userInput+" is not a prime number"));
+}
