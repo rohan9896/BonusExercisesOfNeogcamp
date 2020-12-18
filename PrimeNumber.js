9530d489116748feed4620f0de7fcfb9ea957c7a
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-18 22:35:11 +0530

    Update PrimeNumber.js

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,6 +1,11 @@
 var chalk = require('chalk');
 var readlinesync = require('readline-sync');
 
+
+console.log("\nWhat is Prime number???\n");
+
+console.log("A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers.\n")
+
 function isPrime(n){
     if(n == 1){
         return false;
@@ -17,15 +22,39 @@ function isPrime(n){
     return true;
 }
 
-// console.log(isPrime(13))
+
+function isValidNumber(n){
+    if(typeof n === 'number'){
+        return true;
+    }
+    return false;
+}
+
+// console.log(isPrime("13") && isValidNumber("13"));
+
+var n = 13;
+
+// console.log(typeof n === 'number')
 
 var userInput = readlinesync.question(chalk.bgBlue("Enter the number: "));
 
+// console.log(typeof userInput);
 console.log();
 
+userInput = parseInt(userInput);
+
+var msg = "";
+
 if(isPrime(userInput)){
-    console.log(chalk.blue(userInput+" is a prime number"));
+    msg = userInput+" is a prime number";
 }
 else{
-    console.log(chalk.red(userInput+" is not a prime number"));
+    msg = userInput+" is not a prime number";
+}
+
+if(msg === "NaN is a prime number"){
+    console.log("Enter valid number!!");
+}
+else{
+    console.log(msg);
 }
